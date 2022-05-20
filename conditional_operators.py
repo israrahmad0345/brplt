e2eea5515a324ddb1ff25433970780affcbb120d
Israr Ahmad <israrahmad034@outlook.com>
2022-05-20 12:05:39 -0700

    my firsr commit

---
+++ conditional_operators.py
@@ -0,0 +1,11 @@
+#Logical operators are either "true or false" or "yes or no" or "zero or one"
+print(4==4)
+print(4==6)
+print(4>=2)
+
+#application of logic operators
+age= input("what is your age? ")
+age=int(age)
+school=5
+print(type(age))
+print(age==school)
